486898fd1e7deebb5a16a0bb7672b839a209846c
Bernhard Raml <bernhard.raml@ait.ac.at>
2018-12-09 16:54:31 +0100

    Change window title to "Speckle"

--- speckle/window/main.cpp
+++ speckle/window/main.cpp
@@ -24,7 +24,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Speckle", nullptr, nullptr);
     if (window == nullptr) {
       std::cout << "Failed to create window" << std::endl;
       glfwTerminate();
