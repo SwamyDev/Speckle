a1ad95d238d0e64f47500945844610e5a9899c54
Bernhard Raml <bernhard.raml@ait.ac.at>
2019-01-07 21:27:56 +0100

    Move project to root directory
    As the application project resides now in its own repository it is not
    necessary to keep the speckle library project in its own sub directory.

--- fleck/setup.py
+++ fleck/setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 
 setup(setup_requires=['pytest-runner'],
-      tests_require=['pytest', 'pytest-cov'])
+      tests_require=['pytest', 'pytest-cov', 'PyOpenGL', 'pillow'])
